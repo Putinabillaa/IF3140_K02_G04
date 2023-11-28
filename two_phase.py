08855744949b6b0a0c6dd150f4bcecba4484c2b2
Putinabillaa <aidiraputiabil@gmail.com>
2023-11-29 03:45:29 +0700

    feat: separate exclusive and shared lock

--- two_phase.py
+++ two_phase.py
@@ -4,13 +4,18 @@ class Operation:
         self.operation_type = operation_type
         self.data_item = data_item
 
+class Lock:
+    def __init__(self, transaction_ids, lock_type):
+        self.transaction_ids = transaction_ids
+        self.lock_type = lock_type
+
 class TwoPhaseLocking:
     def __init__(self):
         # operations list
         self.operations = []
 
         # lock table 
-        # key: data item, value: transaction id
+        # key: data item, value: Lock
         self.lock_table = {}
 
         # queued operations
@@ -25,15 +30,15 @@ class TwoPhaseLocking:
     def concurency_control(self):
         while len(self.operations) > 0:
             operation = self.operations.pop(0)
-            if self.is_lock_available(operation):
-                self.lock(operation)
+            if self.lock(operation):
                 self.executed_operations.append(operation)
-                print(f'Operation {operation.operation_type}({operation.data_item}) by T{operation.transaction_id} executed')
+                print(f'T{operation.transaction_id}: Operation {operation.operation_type}({operation.data_item}) executed')
                 if self.is_commit(operation):
                     self.check_queue(operation.transaction_id)
                     self.release_all_lock(operation.transaction_id)
                     print(f'Transaction T{operation.transaction_id} committed')
-            # else:
+            else:
+                print("queued")
                 # TODO: queue or rollback
     
     def check_queue(self, transaction_id):
@@ -48,23 +53,46 @@ class TwoPhaseLocking:
             self.executed_operations.append(transaction_queued_operations)
 
     def lock(self, operation : Operation):
-        print(f'Grant Lock-X({operation.data_item}) to T{operation.transaction_id}')
-        self.lock_table[operation.data_item] = operation.transaction_id
+        if operation.operation_type == 'R':
+            if(operation.data_item in self.lock_table):
+                if(self.lock_table[operation.data_item].lock_type == 'X' and self.lock_table[operation.data_item].transaction_ids[0] != operation.transaction_id):
+                    return False
+                elif(self.lock_table[operation.data_item].lock_type == 'S'):
+                    self.lock_table[operation.data_item].transaction_ids.append(operation.transaction_id)
+                    print(f'Grant Lock-S({operation.data_item}) to T{operation.transaction_id}')
+            else:
+                self.lock_table[operation.data_item] = Lock([operation.transaction_id], 'S')
+                print(f'Grant Lock-S({operation.data_item}) to T{operation.transaction_id}')
+        elif operation.operation_type == 'W':
+            if(operation.data_item in self.lock_table):
+                if(self.lock_table[operation.data_item].lock_type == 'X' and self.lock_table[operation.data_item].transaction_ids[0] != operation.transaction_id):
+                    return False
+                elif(self.lock_table[operation.data_item].lock_type == 'S'):
+                    if(len(self.lock_table[operation.data_item].transaction_ids) == 1 and self.lock_table[operation.data_item].transaction_ids[0] == operation.transaction_id):
+                        self.lock_table[operation.data_item].lock_type = 'X'
+                        print(f'Upgrade Lock-S({operation.data_item}) to Lock-X({operation.data_item}) to T{operation.transaction_id}')
+                    else:
+                        return False
+            else:
+                self.lock_table[operation.data_item] = Lock([operation.transaction_id], 'X')
+                print(f'Grant Lock-X({operation.data_item}) to T{operation.transaction_id}')
+        return True
     
     def release_all_lock(self, transaction_id):
         transaction_locks = []
         for key, value in self.lock_table.items():
-            if value == transaction_id:
+            if transaction_id in value.transaction_ids:
                 transaction_locks.append(key)
         for key in transaction_locks:
-            print(f'Release Lock-X on {key}')
-            del self.lock_table[key]
-
-    def is_lock_available(self, operation : Operation):
-        if operation.data_item in self.lock_table:
-            if self.lock_table[operation.data_item] != operation.transaction_id:
-                return False
-        return True
+            if(self.lock_table[key].lock_type == 'S'):
+                print(f'Release Lock-S on {key} by T{transaction_id}')
+                if(len(self.lock_table[key].transaction_ids) == 1):
+                    del self.lock_table[key]
+                else:
+                    self.lock_table[key].transaction_ids.remove(transaction_id)
+            elif(self.lock_table[key].lock_type == 'X'):
+                print(f'Release Lock-X on {key}')
+                del self.lock_table[key]
     
     def is_commit(self, operation : Operation):
         for op in self.operations:
@@ -86,3 +114,11 @@ class TwoPhaseLocking:
             return True
         except Exception as e:
             return False
+        
+if __name__ == '__main__':
+    two_phase = TwoPhaseLocking()
+    schedule = input('Enter schedule: ')
+    if(two_phase.read_schedule(schedule)):
+        two_phase.concurency_control()
+    else:
+        print('Invalid schedule')
